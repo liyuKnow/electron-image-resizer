5d006c3a60220d8a2668c1c30a9cae73e5730cb1
liyuKnow <kidus2011@gmail.com>
2022-12-01 19:36:02 -0800

    ui setup

--- src/main.js
+++ src/main.js
@@ -1,15 +1,41 @@
 const path = require("path");
 const { app, BrowserWindow } = require("electron");
 
+// const isDev = process.env.NODE_ENV !== "development";
+const isDev = false;
+
+console.log(isDev);
+const isMac = process.platform === "darwin";
+
 function CreateMainWindow() {
   const mainWindow = new BrowserWindow({
-    title: "Image Resizer",
+    title: "Image Resize",
     width: 500,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "./preload/preload.js"),
+    },
   });
+
+  // * isDev
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
   mainWindow.loadFile(path.join(__dirname, "./renderer/screens/index.html"));
 }
 
 app.whenReady().then(() => {
   CreateMainWindow();
+
+  app.on("active", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      CreateMainWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
 });
--- src/renderer/assets/js/renderer.js
+++
@@ -1,1 +0,0 @@
-alert("I am renderer Process and You guys should enjoy!");
