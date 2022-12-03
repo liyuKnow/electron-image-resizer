848b56755abc98999afad760218e11db39ab2522
liyuKnow <kidus2011@gmail.com>
2022-12-03 07:01:58 -0800

    svg icons

--- src/preload/preload.js
+++ src/preload/preload.js
@@ -1,1 +1,11 @@
 console.log("Preload baby!");
+
+
+const { contextBridge } = require('electron');
+
+contextBridge.exposeInMainWorld('versions-api', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  cwd: () => process.cwd(),
+});
