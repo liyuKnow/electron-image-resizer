49fd395939fd62d34a1e886969b799d1e3cab04e
liyuKnow <kidus2011@gmail.com>
2022-12-04 20:05:07 -0800

    toastify, ipc renderer

--- src/preload/preload.js
+++ src/preload/preload.js
@@ -1,18 +1,25 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
-// contextBridge.exposeInMainWorld("versions", {
-//   ping: () => ipcRenderer.invoke("ping"),
-// we can also expose variables, not just functions
-// });
-
 const os = require("os");
 const path = require("path");
+const Toastify = require("toastify-js");
+
 contextBridge.exposeInMainWorld("os", {
   homedir: () => os.homedir(),
 });
 
 contextBridge.exposeInMainWorld("path", {
   join: (...args) => path.join(...args),
+});
+
+contextBridge.exposeInMainWorld("Toastify", {
+  toast: (options) => Toastify(options).showToast(),
+});
+
+contextBridge.exposeInMainWorld("ipcRenderer", {
+  send: (channel, data) => ipcRenderer.send(channel, data),
+  on: (channel, func) =>
+    ipcRenderer.on(channel, (event, ...args) => func(...args)),
 });
 
 console.log("Preload baby!");
@@ -26,4 +33,13 @@ console.log("Preload baby!");
 
 // contextBridge.exposeInMainWorld('os', {
 //   homedir: () => process.versions.node,
+// });
+
+// * IPC SAFE WAY
+
+// const { contextBridge, ipcRenderer } = require("electron");
+
+// contextBridge.exposeInMainWorld("versions", {
+//   ping: () => ipcRenderer.invoke("ping"),
+// we can also expose variables, not just functions
 // });
--- src/renderer/assets/js/renderer.js
+++ src/renderer/assets/js/renderer.js
@@ -1,10 +1,4 @@
-const minimize_btn = document.querySelector("#minimize");
-
-function minimizeWindow() {
-  console.log("minimize this!");
-}
-
-minimize_btn.addEventListener("click", minimizeWindow);
+const { ipcRenderer } = require("electron");
 
 const form = document.querySelector("#img-form");
 const img = document.querySelector("#img");
@@ -13,19 +7,14 @@ const filename = document.querySelector("#filename");
 const heightInput = document.querySelector("#height");
 const widthInput = document.querySelector("#width");
 
-// console.log(`versions.node() => ${versions.node()}`);
-// console.log(`versions.electron() => ${versions.electron()}`);
-// console.log(`versions.node() => ${versions.node()}`);
-// console.log(`versions.platform() => ${versions.platform()}`);
-
-// console.log(`os.homedir() => ${os.homedir()}`);
-// console.log(`os.cwd() => ${os.cwd()}`);
-
 function loadImage(e) {
   const file = e.target.files[0];
 
   if (!isFileAcceptedImage(file)) {
-    alert("ePlease select a valid image with types .jpeg, .jpg or .png!");
+    toastAlert(
+      "Please select a valid image with types .jpeg, .jpg or .png!",
+      "error"
+    );
     return;
   }
   // Get original dimensions
@@ -38,6 +27,35 @@ function loadImage(e) {
 
   form.style.display = "block";
   filename.innerText = file.name;
+
+  outputPath.innerText = path.join(os.homedir(), "Image-resizer");
+}
+
+function sendImage(e) {
+  e.preventDefault();
+
+  const width = widthInput.value;
+  const height = heightInput.value;
+  const imgPath = img.files[o].path;
+
+  if (!img.files[0]) {
+    toastAlert(
+      "Please select a valid image with types .jpeg, .jpg or .png!",
+      "error"
+    );
+    return;
+  }
+
+  if (width === "" && height === "") {
+    toastAlert("Please fill in a height and width", "error");
+    return;
+  }
+  // IPC SEND IMAGE
+  ipcRenderer.send("image:resize", {
+    width,
+    height,
+    imgPath,
+  });
 }
 
 // make sure if file is image
@@ -47,4 +65,37 @@ function isFileAcceptedImage(file) {
   return file && acceptedImageTypes.includes(file["type"]);
 }
 
+function toastAlert(message, alertType) {
+  Toastify.toast({
+    text: message,
+    duration: 5000,
+    close: false,
+    style: {
+      background: alertType == "success" ? "green" : "red",
+      color: "white",
+      textAlign: "center",
+    },
+  });
+}
+
 img.addEventListener("change", loadImage);
+
+form.addEventListener("change", sendImage);
+
+// ======================================================================================================
+
+// const minimize_btn = document.querySelector("#minimize");
+
+// function minimizeWindow() {
+//   console.log("minimize this!");
+// }
+
+// minimize_btn.addEventListener("click", minimizeWindow);
+
+// console.log(`versions.node() => ${versions.node()}`);
+// console.log(`versions.electron() => ${versions.electron()}`);
+// console.log(`versions.node() => ${versions.node()}`);
+// console.log(`versions.platform() => ${versions.platform()}`);
+
+// console.log(`os.homedir() => ${os.homedir()}`);
+// console.log(`os.cwd() => ${os.cwd()}`);
